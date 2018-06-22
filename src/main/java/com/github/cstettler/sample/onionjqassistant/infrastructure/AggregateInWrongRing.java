4e08020e9b3df53faf42c1bdf104772e79c92be8
cstettler <cstettler@imap.ch>
2018-06-22 17:14:23 +0200

    added sample class useful for triggering constraint validation exception (requires uncommenting source code)

---
+++ src/main/java/com/github/cstettler/sample/onionjqassistant/infrastructure/AggregateInWrongRing.java
@@ -0,0 +1,10 @@
+package com.github.cstettler.sample.onionjqassistant.infrastructure;
+
+// TODO enable in order to force constraint violation
+// import com.github.cstettler.sample.onionjqassistant.stereotype.Aggregate;
+
+// TODO enable in order to force constraint violation
+// @Aggregate
+public class AggregateInWrongRing {
+
+}
