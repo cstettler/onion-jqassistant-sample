2bc0720d76c108d04bcb3d66d7104f19ecadf0a8
cstettler <cstettler@imap.ch>
2018-06-22 18:21:21 +0200

    removed commented import (can be added by user on demand)

--- src/main/java/com/github/cstettler/sample/onionjqassistant/infrastructure/AggregateInWrongRing.java
+++ src/main/java/com/github/cstettler/sample/onionjqassistant/infrastructure/AggregateInWrongRing.java
@@ -1,9 +1,6 @@
 package com.github.cstettler.sample.onionjqassistant.infrastructure;
 
 // TODO enable in order to force constraint violation
-// import com.github.cstettler.sample.onionjqassistant.stereotype.Aggregate;
-
-// TODO enable in order to force constraint violation
 // @Aggregate
 public class AggregateInWrongRing {
 
